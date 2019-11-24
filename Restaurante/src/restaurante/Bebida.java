36687dbe165ddc912e943a9ae24aa5d7052e6778
Titovare <carlosjvare@gmail.com>
2019-11-23 21:01:36 -0600

    Correcciones 2

--- Restaurante/src/restaurante/Bebida.java
+++ Restaurante/src/restaurante/Bebida.java
@@ -9,7 +9,7 @@ import java.util.*;
 public class Bebida extends Platillo {
 
     public Bebida(boolean Gaseosa, String Codigo, String Descripcion, int Racion, float Calorias, int Precio, boolean activo, int Ventas, String nombre) {
-        super(Codigo, Descripcion, Racion, Calorias, Precio, activo, Ventas, nombre);
+        super(Codigo, Descripcion, Racion, Calorias, Precio, nombre);
         this.Gaseosa = Gaseosa;
     }
     
