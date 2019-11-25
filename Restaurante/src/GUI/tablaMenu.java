a7120273a2ac709a76f183768d95fc990771db97
TilapiaKIddo2 <jostenoriop@gmail.com>
2019-11-25 17:46:55 -0600

    Arreglo menu

--- Restaurante/src/GUI/tablaMenu.java
+++ Restaurante/src/GUI/tablaMenu.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package GUI;
+import java.util.ArrayList;
 import restaurante.*;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
@@ -15,12 +16,19 @@ import javax.swing.table.*;
  * @author Personal
  */
 public class tablaMenu extends javax.swing.JFrame implements Observer{
-
+    
+    ArrayList <String> tipos = new ArrayList();
+    boolean habilitado1 = false;
+    boolean habilitado2 = true;
     /**
      * Creates new form tablaMenu
      */
     public tablaMenu() {
         initComponents();
+        tipos.add("PTR");
+        tipos.add("BEB");
+        tipos.add("ENT");
+        tipos.add("PRN");
         this.update();
     }
 
@@ -36,6 +44,9 @@ public class tablaMenu extends javax.swing.JFrame implements Observer{
         jLabel2 = new javax.swing.JLabel();
         jScrollPane4 = new javax.swing.JScrollPane();
         TablaMenu = new javax.swing.JTable();
+        jComboBox1 = new javax.swing.JComboBox<>();
+        jComboBox2 = new javax.swing.JComboBox<>();
+        jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Menu del restaurante");
@@ -71,6 +82,30 @@ public class tablaMenu extends javax.swing.JFrame implements Observer{
         TablaMenu.setRowHeight(30);
         jScrollPane4.setViewportView(TablaMenu);
 
+        jComboBox1.setFont(new java.awt.Font("Vani", 0, 14)); // NOI18N
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Todos", "Entrada", "Plato fuerte", "Postre", "Bebida" }));
+        jComboBox1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jComboBox1ActionPerformed(evt);
+            }
+        });
+
+        jComboBox2.setFont(new java.awt.Font("Vani", 0, 14)); // NOI18N
+        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Ambos", "Habilitados", "Deshabilitados" }));
+        jComboBox2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jComboBox2ActionPerformed(evt);
+            }
+        });
+
+        jButton3.setFont(new java.awt.Font("Vani", 0, 14)); // NOI18N
+        jButton3.setText("Filtrar");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -78,25 +113,99 @@ public class tablaMenu extends javax.swing.JFrame implements Observer{
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jScrollPane4, javax.swing.GroupLayout.DEFAULT_SIZE, 701, Short.MAX_VALUE)
-                        .addContainerGap())
+                    .addComponent(jScrollPane4)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 277, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                        .addGap(49, 49, 49)
+                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton3)
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 278, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                    .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                    .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 319, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jComboBox1ActionPerformed
+
+    private void jComboBox2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jComboBox2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        String tipo = jComboBox1.getSelectedItem().toString();
+        if (null == tipo){
+            tipos.clear();
+            tipos.add("PTR");
+            tipos.add("BEB");
+            tipos.add("ENT");
+            tipos.add("PRN");
+        }
+        else switch (tipo) {
+            case "Entrada":
+                tipos.clear();
+                tipos.add("ENT");
+                break;
+            case "Plato fuerte":
+                tipos.clear();
+                tipos.add("PRN");
+                break;
+            case "Postre":
+                tipos.clear();
+                tipos.add("PTR");
+                break;
+            case "Bebida":
+                tipos.clear();
+                tipos.add("BEB");
+                break;
+            default:
+                tipos.clear();
+                tipos.add("PTR");
+                tipos.add("BEB");
+                tipos.add("ENT");
+                tipos.add("PRN");
+                break;
+        }
+        String habil = jComboBox2.getSelectedItem().toString();
+        if (null == habil){
+             habilitado1 = false;
+             habilitado2 = true;
+        }
+        else switch (habil) {
+            case "Habilitados":
+                habilitado1 = true;
+                 habilitado2 = true;
+                break;
+            case "Deshabilitados":
+                habilitado1 = false;
+                habilitado2 = false;
+                break;
+            default:
+                habilitado1 = false;
+                habilitado2 = true;
+                break;
+        }
+        update();
+    }//GEN-LAST:event_jButton3ActionPerformed
     
     @Override
     public void update () {
@@ -105,21 +214,23 @@ public class tablaMenu extends javax.swing.JFrame implements Observer{
             model.removeRow(model.getRowCount()-1);
         }
         for (Platillo plato : Restaurante.getInstance().getMenu()){
-            String[] fila;
-            fila = new String[7];
-            fila[0] = plato.getCodigo();
-            fila[1] = plato.getNombre();
-            fila[2] = plato.getDescripcion();
-            fila[3] =Integer.toString(plato.getRacion());
-            fila[4] =Float.toString(plato.getCalorias());
-            fila[5] =Integer.toString(plato.getPrecio());
-            if (plato.isActivo()){
-                fila[6] = "Si";
+            if ((tipos.contains(plato.getCodigo().substring(0,3))) && ((habilitado1 == plato.isActivo())||(habilitado2 == plato.isActivo()))){
+                String[] fila;
+                fila = new String[7];
+                fila[0] = plato.getCodigo();
+                fila[1] = plato.getNombre();
+                fila[2] = plato.getDescripcion();
+                fila[3] =Integer.toString(plato.getRacion());
+                fila[4] =Float.toString(plato.getCalorias());
+                fila[5] =Integer.toString(plato.getPrecio());
+                if (plato.isActivo()){
+                    fila[6] = "Si";
+                }
+                else{
+                    fila[6] = "No";
+                }
+                model.addRow(fila);
             }
-            else{
-                fila[6] = "No";
-            }
-            model.addRow(fila);   
         }
         TablaMenu.setModel(model);
         TablaMenu.updateUI();
@@ -162,6 +273,9 @@ public class tablaMenu extends javax.swing.JFrame implements Observer{
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTable TablaMenu;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JComboBox<String> jComboBox2;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane4;
     // End of variables declaration//GEN-END:variables
